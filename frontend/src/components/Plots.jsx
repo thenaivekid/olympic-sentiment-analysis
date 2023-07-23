49c5c7d777d2d91875227e4ca6572757750a1177
thenaivekid <neupane.ashok.9696@gmail.com>
2023-07-23 14:37:50 +0545

    barchart copy and pasted to plots to resolve import barchart error

--- frontend/src/components/Plots.jsx
+++ frontend/src/components/Plots.jsx
@@ -1,5 +1,30 @@
 import PieChart from "./PieChart";
-import  BarChart  from './Barchart';
+
+
+const BarChart = ({ chartData, title, no_of_tweets }) => {
+    return (
+      <div className="chart-container">
+        <h2 style={{ textAlign: "center" }}>{ title }</h2>
+        <h3>Total tweets: { no_of_tweets }</h3>
+        <Bar
+          data={chartData}
+          options={{
+            plugins: {
+              title: {
+                display: false,
+                text: ''
+              },
+              legend: {
+                display: false
+              }
+            }
+          }}
+        />
+      </div>
+    );
+  };
+
+
 const Plots = ({ chartData, title, no_of_tweets }) => {
     return (
             <div className="sent_card_plots">
