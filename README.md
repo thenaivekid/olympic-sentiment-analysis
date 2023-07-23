eb4cff53b34caf19d9a5ff813d9b7d864fc45879
thenaivekid <neupane.ashok.9696@gmail.com>
2023-07-23 16:28:44 +0545

    doc added in readme. standard maintained in backend code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tweeter analysis
+# Social Media Analysis
 
 Try it out:
 https://olympic-sentiment-analysis-gdgyrsntw-thenaivekid.vercel.app/
@@ -8,12 +8,57 @@ https://youtu.be/5lwiuEWD7ko
 
 <img src="ss.png">
 
-An easy to use app to visualize what people are talking on socialn  media during an occassion eg Olypic.
+# Documentation
 
-## Features to be added:
+## Working with data
+
+An easy to use app to visualize what people are talking on socialn  media during an Oympics.
+
+<!-- ## Features to be added:
 
 - see visualizations according to the locations of audiences
-- feature to let use specify the type of sentiment and number of tweets to read the tweets
+- feature to let use specify the type of sentiment and number of tweets to read the tweets -->
+
+
+For the sake of demonstration of the app, instead of getting live posts from the social media APIs, we used data of tokyo olympics 2020 that is found <a href="https://www.kaggle.com/datasets/amritpal333/tokyo-olympics-2021-tweets">here</a>
+
+The EDA can be found in the `tokyo_olympics0.ipynb` file. The sentiments were predicted using hugging face pretrained model.
+```python
+from transformers import pipeline
+
+sent_pipeline = pipeline("sentiment-analysis", 
+    model='finiteautomata/bertweet-base-sentiment-analysis'
+    )   
+```
+This model gives {sentiment, sentiment_score} for the input text. EG.
+```
+('NEG', 0.7984506487846375)
+```
+Precomputed sentiment and score columns are added to dataset for fast accessing and scalability. Now the data and the sentiments are ready to be consumed by the web app.
+
+## Web app 
+
+### Backend
+
+We have used Fast API for making our web app faster.
+We have three API endpoints:
+- `/overall`: It takes get request and returns title, total number of posts and a list with number of posts with negative, neutral and positive sentiments respectively.
+- `/sentiments`: It takes keyword and  filters all the rows with the given keyword . It also returns title, total number of posts and a list with number of posts with negative, neutral and positive sentiments respectively.
+- `/tweets`: It takes keyword, number of posts and sentiment of the posts and returns the filtered posts.
+
+
+### Frontend
+
+We have written frontend on react js and used chart js for visualization.
+In the `src/components` we have:
+- `BarGraph`
+- `PieChart`
+- `Plots`: It puts the charts together.
+- `Sent`: It creates a card for an individual keyword query
+- `Sents`: It contains all the `Sent` items
+- `Tweets`: It shows the actual posts from the social media
+- `Search`: It has a input field to take the keyword and a submit button to call the API for the keyword.
+
 
 ## Usage
 
@@ -35,4 +80,5 @@ npm run dev
 
 # live 
 backend: https://social-media-ashok.onrender.com/greet/
+<br>
 frontend: https://olympic-sentiment-analysis-gdgyrsntw-thenaivekid.vercel.app/
--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,55 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import pandas as pd
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 df = pd.read_csv('data_small.csv')
+
 class KeywordRequest(BaseModel):
     query: str
-    
 
 class TweetsRequest(BaseModel):
     query: str
     no_of_tweets: int
     sentiment: int
+
+@app.get('/greet/')
+def greet():
+    return {'greeting': 'Hello, world!'}
+
+@app.get('/overall')
+def overall_sentiment():
+    sentiment_counts = df['num_labels'].value_counts().to_dict()
+    return {
+        'title': 'Overall Sentiment',
+        'no_of_tweets': df.shape[0],
+        'count': sentiment_counts
+    }
+
+@app.post("/sentiments")
+def process_text(request: KeywordRequest):
+    filtered_df = df[df['text'].str.contains(request.query, case=False)]
+    sentiment_counts = filtered_df['num_labels'].value_counts().to_dict()
+    return {
+        'title': request.query,
+        'no_of_tweets': filtered_df.shape[0],
+        'count': sentiment_counts
+    }
+
+@app.post('/tweets')
+def show_tweets(request: TweetsRequest):
+    filtered_df = df[df['text'].str.contains(request.query, case=False)]
+    
+    if request.sentiment in range(3):
+        filtered_df = filtered_df[filtered_df['num_labels'] == request.sentiment]
+    
+    tweets = filtered_df['text'].head(request.no_of_tweets).tolist()
+    if tweets:
+        return {'tweets': tweets}
+    else:
+        raise HTTPException(status_code=404, detail='Resources not found.')
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,33 +57,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.get('/greet/')
-def greet():
-    print()
-    return {'geeting': 'Hello world'}
-    
-@app.get('/overall')
-def overall_sentiment():
-    count = df['num_labels'].value_counts()
-    return JSONResponse(content={'title': 'overall', 'no_of_tweets': df.shape[0], 'count': list(count)}, status_code=200)
-
-@app.post("/sentiments")
-def process_text(request: KeywordRequest):
-    print(request.query)
-    filtered_df = df[df['text'].str.contains(request.query, case=False)]
-    count = filtered_df['num_labels'].value_counts()
-    return JSONResponse(content={'title': request.query, 'no_of_tweets': filtered_df.shape[0], 'count': list(count)}, status_code=200)
-
-@app.post('/tweets')
-def show_tweets(request: TweetsRequest):
-    """request should contain keyword to filter, no of tweets, neg/pos/neu/any. returns the actual tweets"""
-    filtered_df = df[df['text'].str.contains(request.query, case=False)]
-    if 0 <= request.sentiment <= 2:
-        filtered_df = filtered_df[filtered_df['num_labels'] == request.sentiment]
-    tweets = list(filtered_df.head(request.no_of_tweets)['text'])
-    if tweets:
-        print(tweets)
-        return JSONResponse(content={'tweets': tweets}, status_code=200)
-    
-    return JSONResponse(content='resources not found error', status_code=400)
